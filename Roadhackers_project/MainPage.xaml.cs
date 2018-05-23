c5a70d6f192b6b04e4bc429ead3d6c3df356c5b2
Sandra <r0711012@student.thomasmore.be>
2018-05-23 19:37:08 +0100

    GUI version 1 with API weather, will be extended. First version just with temperature

--- Roadhackers_project/MainPage.xaml.cs
+++ Roadhackers_project/MainPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Roadhackers_project.Model;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -16,15 +19,37 @@ using Windows.UI.Xaml.Navigation;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Roadhackers_project
+
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
+
     {
+
         public MainPage()
         {
             this.InitializeComponent();
+            getWeatherData();
+
+            
         }
+
+        async void getWeatherData()
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=antwerp,be&units=metric&apikey=23c1237f8e3a4a8eca581f9c2c647d85"; //SRS:  URL to fetch data with my personal generated API Key
+            HttpClient client = new HttpClient(); // SRS: to call the website 
+
+            string weatherData = await client.GetStringAsync(url); // SRS: to save the fetched data into a string, data will be in json format and has to be converted
+            var data = JsonConvert.DeserializeObject<Rootobject>(weatherData);
+
+            txtBlockWeatherResult.Text = data.main.temp.ToString() + "C";
+
+
+        }
+
+       
     }
 }
+
